03c8f3571a916d1abea4f9da83d974f1f4d99883
Jose Esteban <josegg1897@gmail.com>
2021-03-24 08:16:08 -0400

    Add useRef

--- fetch-practice/src/App.js
+++ fetch-practice/src/App.js
@@ -1,10 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import './App.css';
 import Card from './Card';
 
 function App() {
+  const inputRef = useRef(null)
   const [posts, setPosts] = useState([])
+  const [inputValue, setInputValue] = useState(null)
   useEffect(() => {
     const fetchPosts = async() => {
       try {
@@ -17,15 +19,32 @@ function App() {
     }
     fetchPosts();
   })
+  const handleChange = (e) => {
+    setInputValue(inputRef.current.value);
+  }
   return (
-    <div className="App">
-      {
-        posts.map( (post) => {
-          const {userId, id, title, body} = post
-          return <Card userId = {userId} id = {id} title = {title} body = {body} key={id}/>
-        })
-      }
-    </div>
+    <React.Fragment>\
+      <center>
+        <label>User ID: </label>
+        <input ref={inputRef} type="number" onChange={handleChange} style={{margin: '0 auto'}}/>
+      </center>
+      <div className="App">
+        {
+          inputValue ?
+            posts.map( (post) => {
+              const {userId, id, title, body} = post 
+              if(userId.toString() === inputValue) {
+                return <Card userId = {userId} id = {id} title = {title} body = {body} key={id}/>
+              }
+              return null;
+            }) :
+            posts.map( (post) => {
+              const {userId, id, title, body} = post
+                return <Card userId = {userId} id = {id} title = {title} body = {body} key={id}/>
+            })
+        }
+      </div>
+    </React.Fragment>
   );
 }
 
